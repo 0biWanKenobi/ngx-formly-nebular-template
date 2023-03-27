93375d11de13e703b6677d05250e043bb307f808
0biWanKenobi <metal.rulesall@gmail.com>
2023-03-27 11:07:30 +0200

    chore: organize imports

--- src/app/pages/form/form.module.ts
+++ src/app/pages/form/form.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { NbButtonModule, NbLayoutModule } from "@nebular/theme";
+import { FormlyModule } from "@ngx-formly/core";
 
 import { FormlyFieldInputModule } from "src/app/ui/input/formly-field-input.module";
-import { NbButtonModule, NbLayoutModule } from "@nebular/theme";
 import { FormlyFieldSelectModule } from "src/app/ui/select/formly-field-select.module";
 import { BootstrapWrapperModule } from "src/app/ui/wrappers/bootstrap-wrapper/bootstrap-wrapper.module";
 import { ShowCaseFormComponent } from "./form.component";
-import { CommonModule } from "@angular/common";
-import { FormlyModule } from "@ngx-formly/core";
 import { DataService } from "src/app/service/service";
 
 @NgModule({
